b03280628d257b4adb9ece3d0bc00d6976d3e281
Gabriel Simon <gabrielsimon775@gmail.com>
2022-08-17 22:55:33 -0300

    First commit backend

---
+++ src/main/java/yard/truckdock/core/entities/Login.java
@@ -0,0 +1,9 @@
+package yard.truckdock.core.entities;
+
+import java.io.Serializable;
+
+public class Login implements Serializable {
+    private Long id;
+    private String user;
+    private String password;
+}
---
+++ src/test/java/yard/truckdock/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package yard.truckdock.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
