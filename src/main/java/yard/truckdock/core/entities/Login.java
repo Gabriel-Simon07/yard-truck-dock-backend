cd595779f97926b129ae129a06cfd9a0d8f62d64
Gabriel Simon <gabrielsimon775@gmail.com>
2022-08-27 15:58:24 -0300

    Create table login

--- src/main/java/yard/truckdock/core/entities/Login.java
+++ src/main/java/yard/truckdock/core/entities/Login.java
@@ -1,9 +1,61 @@
 package yard.truckdock.core.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity()
+@Table(name = "tb_login")
 public class Login implements Serializable {
-    private Long id;
-    private String user;
-    private String password;
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String user;
+	
+	public Login() {}
+	
+	public Login(Long id, String user) {
+		this.id = id;
+		this.user = user;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		return Objects.equals(id, other.id) && Objects.equals(user, other.user);
+	}
 }
